0d28d553f9abfc866ba55a1f1b6b8a16e7e40432
kstg5248068 <wangneng_1986@163.com>
2019-04-10 23:37:27 +0800

    测试view
    测试view

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,12 +10,68 @@ Page({
     //   'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
     //   'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     // ],
-    imgUrls:[
+    imgUrls_h: [
       '../../image/res/tu1.png',
       '../../image/res/tu2.png',
       '../../image/res/tu3.png',
       '../../image/res/tu4.png',
     ],
+    list: [
+      {
+        id: "zu1",
+        name: '组1',
+        detail: [{
+          idd: "1",
+          url: '../../image/res/tu1.png',
+        }, {
+          idd: "2",
+          url: '../../image/res/tu2.png',
+        }, {
+          idd: "3",
+          url: '../../image/res/tu3.png',
+        }, {
+          idd: "4",
+          url: '../../image/res/tu4.png',
+        }]
+      },
+      {
+        id: "zu2",
+        name: '组2',
+        detail: [{
+          idd: "4",
+          url: '../../image/res/tu1.png',
+        }, {
+          idd: "5",
+          url: '../../image/res/tu2.png',
+        }, {
+          idd: "6",
+          url: '../../image/res/tu3.png',
+        }, {
+          idd: "7",
+          url: '../../image/res/tu4.png',
+        }]
+      },
+      {
+        id: "zu3",
+        name: '组3',
+        detail: [{
+          idd: "8",
+          url: '../../image/res/tu1.png',
+        }, {
+          idd: "9",
+          url: '../../image/res/tu2.png',
+        }, {
+          idd: "10",
+          url: '../../image/res/tu3.png',
+        }, {
+          idd: "11",
+          url: '../../image/res/tu4.png',
+        }]
+      }
+     ]
+
+    ,
+
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
@@ -28,7 +84,11 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-  }
- 
+  click:function(e){
+    var tag = e.target;
+    //var id=tag.idd;
+    console.log(tag.id)
+  },
+  onLoad: function() {}
+
 })
